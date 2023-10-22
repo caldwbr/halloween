4466a3a3bfb36800c2cbb7d9218c962cf7619a2e
caldwbr <caldwbr@gmail.com>
2023-10-22 14:22:10 -0500

    Initial Commit

---
+++ halloween/halloweenApp.swift
@@ -0,0 +1,17 @@
+//
+//  halloweenApp.swift
+//  halloween
+//
+//  Created by Brad Caldwell on 10/22/23.
+//
+
+import SwiftUI
+
+@main
+struct halloweenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
